cf05f0ce0b5b19b67944402bbac21a8a082db347
Fakhour Mohammed <mohammed.fakhour@babel.es>
2021-11-17 19:58:17 +0100

    Share a Candy with an Intent - add createShareContent() method in onOptionsItemSelected

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -77,12 +77,13 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        createShareIntent();
         return super.onOptionsItemSelected(item);
     }
 
 
     public void createShareIntent(){
-        Intent shareIntent=new Intent(Intent.ACTION_SEND);
+        Intent shareIntent =new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
         startActivity(shareIntent);
